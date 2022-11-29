2f4a8d0d6b3fcca83944caaaf376e18c32aeb46c
atnardo <atnardo@gmail.com>
2022-11-28 20:55:36 -0500

    added lists

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 
 
+consonants = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','z']
+vowels = ['a','e','i','o','u']
 i = input("Input two words:")
 print("\nYour words: " + i)
 
